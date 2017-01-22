ab50e029017803ee5aa630dd37819bb722949788
Simon Bothén <caresilabs@gmail.com>
2017-01-22 15:45:53 +0100

    Final push

---
+++ Roadtothewhitehouse/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+    private float time;
+
+	//// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+
+        if (time >= 6.8f)
+        {
+            SceneManager.LoadScene(1);
+        }
+	}
+}
